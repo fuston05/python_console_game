d8cc044d8f6e1c597244e4c11a14d2a073b14773
fuston05 <fuston05@yahoo.com>
2020-06-16 08:00:32 -0400

    adds a useEffect.py file, cleans up item use in item.py

--- src/existingItems.py
+++ src/existingItems.py
@@ -2,10 +2,15 @@ from item import Item
 
 item = {
     'book': Item('book', "Very old and dusty, you can't make out the writing on the cover"),
+
     'sword': Item('sword', 'Rusty old sword'),
     'key': Item('key', 'Shiny gold skeleton key'),
+
     'chest': Item('chest', 'Dusty old locked chest'),
+
     'torch': Item('torch', 'Used wooden torch'),
+
     'bag': Item('bag', 'It\'s full of gold! Looks to be about 25GP.'),
-    'portal': Item('portal', 'a portal of glowing blue light', is_useable=True, use_effect='port outside'),
+
+    'portal': Item('portal', 'a portal of glowing blue light', is_useable=True, use_effect= 'port outside'),
 }
--- src/player.py
+++ src/player.py
@@ -1,5 +1,3 @@
-# Write a class to hold player information, e.g. what room they are in
-# currently.
 from existingRooms import room
 
 
